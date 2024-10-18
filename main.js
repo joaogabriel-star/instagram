36d7d23ccb21c49c7e8d0641c70c19cb6c338cc9
joaogabriel-star <joaogabrielsouza706@gmail.com>
2024-10-17 22:23:39 -0300

    insta

---
+++ main.js
@@ -0,0 +1,4 @@
+document.getElementById('alertaMarcados').addEventListener('click', function(event) {
+  event.preventDefault(); // Evita que o link siga seu caminho padrão
+  alert('O site está em manutenção');
+});
