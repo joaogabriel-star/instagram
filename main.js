639dcd988c67b43ccb52ba64e3cf9f6d87be3677
joaogabriel-star <joaogabrielsouza706@gmail.com>
2024-10-18 19:53:21 -0300

    fixing

--- main.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('alertaMarcados').addEventListener('click', function(event) {
-  event.preventDefault(); // Evita que o link siga seu caminho padrão
-  alert('O site está em manutenção');
-});
